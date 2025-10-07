ac3259b49d577f4472340073946e44745ec3735e
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-10-07 15:58:36 +0300

    Make Genaric Repositry instead of make repositry for each Entity

---
+++ GymDataAccsess/Repositres/Classes/GenaricRpositry.cs
@@ -0,0 +1,47 @@
+﻿using GymDataAccsess.Data;
+using GymDataAccsess.Models;
+using GymDataAccsess.Repositres.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccsess.Repositres.Classes
+{
+    internal class GenaricRpositry<TModel> : IRepositryGenaric<TModel> where TModel : BaseEntities, new()
+    {
+        private readonly GymDbContext dbContext;
+
+        public GenaricRpositry(GymDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public int Add(TModel model)
+        {
+            dbContext.Set<TModel>().Add(model); 
+            return dbContext.SaveChanges();
+        }
+
+        public int Delete(TModel model)
+        {
+            dbContext.Set<TModel>().Remove(model);
+            return dbContext.SaveChanges();
+        }
+
+        public TModel? GetById(int id) => dbContext.Set<TModel>().Find(id);
+               
+        
+
+        public IEnumerable<TModel> GetAll() =>dbContext.Set<TModel>().AsNoTracking().ToList();
+      
+
+        public int Update(TModel model)
+        {
+
+           dbContext.Set<TModel>().Update(model);
+            return dbContext.SaveChanges();
+        }
+    }
+}
---
+++ GymDataAccsess/Repositres/Interfaces/IRepositryGenaric.cs
@@ -0,0 +1,26 @@
+﻿using GymDataAccsess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDataAccsess.Repositres.Interfaces
+{
+    public interface IRepositryGenaric<TEntity> where TEntity: BaseEntities 
+    {
+
+        TEntity? GetById(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+
+        int Add(TEntity model);
+
+
+        int Update(TEntity model);
+
+
+        int Delete(TEntity model);
+    }
+}
