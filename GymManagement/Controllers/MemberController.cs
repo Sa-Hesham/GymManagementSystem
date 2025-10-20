fdd76a8c5b1b9f31bae01975e4e1080c14b33c52
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-10-21 01:37:30 +0300

    Add CreatedMember View

--- GymManagement/Controllers/MemberController.cs
+++ GymManagement/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using GymBusinessLogic.Services.Interfaces;
+using GymBusinessLogic.ViewModels.MemberViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.Controllers
@@ -22,19 +23,27 @@ namespace GymManagement.Controllers
             
             if (id <= 0)
             {
+                TempData["ErrorMassege"] = "Id Must Be not negative or 0 ";
                 return RedirectToAction(nameof(Index));
             }
 
             var member = _members.GetMemberDetails(id);
-        
-        
-         return View(member);
+
+            if (member is null)
+            {
+                TempData["ErrorMassege"] = "member not found  ";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(member);
         }
 
 
         public IActionResult showMemberHelthRecord(int id)
         {
             if (id <= 0) {
+
+                TempData["ErrorMassege"] = "Id Must Be not negative or 0 ";
 
                 return RedirectToAction(nameof(Index));
             
@@ -43,6 +52,30 @@ namespace GymManagement.Controllers
             var memberHelathrecord = _members.HelthRecordMember(id);
 
             return View(memberHelathrecord);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateMember(CreateMemberViewModel CreatedMember)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("InvalidData", "Cheak Data And Missing Field");
+                return View(nameof(Create),CreateMember);
+            }
+           bool  IsCreated =  _members.CreateMember(CreatedMember);
+            if (!IsCreated)
+                TempData["ErrorMassege"] = "Member not Created ";
+            else
+                TempData["SuccsessMassege"] = "Member Created successfully ";
+
+            return RedirectToAction(nameof(Index));
+
+
+
         }
     }
 }
