aa3efeba232a7d3f6e7299ed84e498f7e12b2e9a
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-10-23 04:58:16 +0300

    add session

---
+++ GymManagement/Controllers/SessionController.cs
@@ -0,0 +1,55 @@
+﻿using GymBusinessLogic.Services.Clasess;
+using GymBusinessLogic.Services.Interfaces;
+using GymDataAccsess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly ISeesionService _seesionService;
+      
+
+        public SessionController(ISeesionService seesionService)
+        {
+            _seesionService = seesionService;
+        }
+        public IActionResult Index()
+        {
+          var sessions=  _seesionService.GetAllSessions();
+            return View(sessions);
+        }
+
+
+
+        public IActionResult Details(int id) {
+
+            if (id <= 0)
+            {
+                TempData["ErrorMassege"] = "Id Must Be not negative or 0 ";
+                return RedirectToAction(nameof(Index));
+            }
+            var session= _seesionService.GetSessionById(id);
+
+            if (session == null)
+            {
+
+                TempData["ErrorMassege"] = " session not Found ";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(session);
+        
+        }
+
+
+
+        public IActionResult Create() { 
+        
+        
+        
+        return View();  
+        
+        }
+    }
+}
